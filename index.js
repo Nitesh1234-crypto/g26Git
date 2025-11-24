bfb2a230a093341d21437dd2265740702b4d686d
Nitesh <nitesh.e21@nsut.ac.in>
2025-11-24 14:53:32 +0530

    created index.js file

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.get("/",(req,res)=>{
+    res.json({message:"ok"})
+})
+mongoose.connect("mongod://127.0.0.1:271017/test")
+.then(()=>console.log("db connected"))
+app.listen(5565,()=>{
+    console.log("server started")
+}
+)
